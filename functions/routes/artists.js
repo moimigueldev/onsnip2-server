ca56453418003580562e2d07cb15425582fefdfe
moises <moimigueldev@gmail.com>
2020-03-22 14:18:42 -0600

    able to follow and unfollow artist based on id

--- functions/routes/artists.js
+++ functions/routes/artists.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const rp = require('request-promise');
 const keys = require('../secret-keys/env-vars');
 
-router.post('/artist', (req, res) => {
+router.post('/artist', async (req, res) => {
     const token = req.body.token
     const id = req.body.id;
 
-    const playlistOptions = {
+    const artistOptions = {
         url: `https://api.spotify.com/v1/artists/${id}`,
         headers: {
             'Authorization': `Bearer ${token}`,
@@ -14,12 +14,56 @@ router.post('/artist', (req, res) => {
         }
     };
 
-    rp(playlistOptions).then((response) => {
-        res.send({ artist: JSON.parse(response) })
+    const isFollowingOptions = {
+        url: `https://api.spotify.com/v1/me/following/contains?type=artist&ids=${id}`,
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Access-Control-Allow-Origin': keys.cors['spotify-header']
+        }
+    };
+
+    const artist = await rp(artistOptions)
+    const isFollowing = await rp(isFollowingOptions)
+
+    res.send({ artist: JSON.parse(artist), following: isFollowing })
+});
+
+router.post('/follow-artist', (req, res) => {
+    const token = req.body.token
+    const id = req.body.id;
+
+
+    const followArtistOptions = {
+        url: `https://api.spotify.com/v1/me/following?type=artist&ids=${id}`,
+        method: "PUT",
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Access-Control-Allow-Origin': keys.cors['spotify-header']
+        }
+    };
+
+    rp(followArtistOptions).then((response) => {
+        res.send({ artistResponse: response })
     })
+})
+
+router.post('/unfollow-artist', (req, res) => {
+    const token = req.body.token
+    const id = req.body.id;
 
 
+    const unfollowArtistOptions = {
+        url: `https://api.spotify.com/v1/me/following?type=artist&ids=${id}`,
+        method: "DELETE",
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Access-Control-Allow-Origin': keys.cors['spotify-header']
+        }
+    };
 
+    rp(unfollowArtistOptions).then((response) => {
+        res.send({ artistResponse: response })
+    })
 })
 
 
