4a1138e6a2ae806333e6681ca6924ada0cc661fe
moises <moimigueldev@gmail.com>
2020-03-23 18:14:19 -0600

    added top tracks route

--- functions/routes/tracks.js
+++ functions/routes/tracks.js
@@ -29,6 +29,26 @@ router.post('/track', async (req, res) => {
         trackFeatures: JSON.parse(trackFeatures)
     })
 
+});
+
+
+router.post('/top-tracks', (req, res) => {
+    const token = req.body.token
+    const id = req.body.id;
+    const time = req.body.time
+    console.log('top tracks', time);
+
+    const topTracksOptions = {
+        url: `https://api.spotify.com/v1/me/top/tracks?time_range=${time}&limit=50`,
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Access-Control-Allow-Origin': keys.cors['spotify-header']
+        }
+    };
+
+    rp(topTracksOptions).then(response => {
+        res.send({ tracks: JSON.parse(response) })
+    })
 })
 
 module.exports = router
