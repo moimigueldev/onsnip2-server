1254b3ad350f55ee493a68538741af56ac79953d
moises <moimigueldev@gmail.com>
2020-03-23 19:23:19 -0600

    added user playlist url

--- functions/routes/playlist.js
+++ functions/routes/playlist.js
@@ -20,7 +20,42 @@ router.post('/playlistId', (req, res) => {
 
         res.send(JSON.parse(response))
     })
+})
 
+router.post('/all-playlist', (req, res) => {
+    const token = req.body.token;
+
+
+    const playlistOptions = {
+        url: `https://api.spotify.com/v1/me/playlists?limit=50`,
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Access-Control-Allow-Origin': keys.cors['spotify-header']
+        }
+    };
+
+    rp(playlistOptions).then(response => {
+        res.send({ playlist: JSON.parse(response) })
+    })
+
+})
+
+router.post('/user-playlist', (req, res) => {
+    const token = req.body.token;
+    const id = req.body.id
+
+
+    const playlistOptions = {
+        url: `	https://api.spotify.com/v1/playlists/${id}`,
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Access-Control-Allow-Origin': keys.cors['spotify-header']
+        }
+    };
+
+    rp(playlistOptions).then(response => {
+        res.send({ playlist: JSON.parse(response) })
+    })
 
 })
 
